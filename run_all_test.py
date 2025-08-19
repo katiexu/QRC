fc6f397c0789834c573c0ff0fc52695f072fe0a5
katie <xuke0531@hotmail.com>
2025-08-19 09:41:32 +0800

    HEILS_QRC_Mnist

--- run_all_test.py
+++ run_all_test.py
@@ -11,9 +11,9 @@ for dataset_name in ['mnist01','mnist36','mnist4''mnist6','mnist8','mnist10']:
     X_test, y_test = test_datasets
     X_train = X_train.reshape(len(X_train), -1)
     X_test = X_test.reshape(len(X_test), -1)
-    for name,Model in {'qubit':weakMeasure_mnist.SeparableVariationalClassifier,
-                  'feature': weakMeasure_mnist2.SeparableVariationalClassifier,
-                  'features': weakMeasure_mnist3.SeparableVariationalClassifier }.items():
+    for name,Model in {'qubit':heils_qrc_end_qubit.SeparableVariationalClassifier,
+                  'feature': heils_qrc_end_feature.SeparableVariationalClassifier,
+                  'features': heils_qrc_end_all_features.SeparableVariationalClassifier }.items():
         model = Model(jit=True, max_vmap=32, n_layers=4, n_qubits=6, n_classes=4)
         model.fit(X_train, y_train)
         train_predictions = np.array(model.predict(X_train))
